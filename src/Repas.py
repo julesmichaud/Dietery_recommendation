2fcc1ef083e4cf1739cec669eb8cfb1c1834b3da
Guerrini <valerio.guerrini@telecom-paris.fr>
2022-06-20 14:26:50 +0200

    test2

---
+++ src/Repas.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 20, 2022
+
+@author: valerio
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
