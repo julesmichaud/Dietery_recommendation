84ae595259367a733aecb453905fc479f2cd94fb
Guerrini <valerio.guerrini@telecom-paris.fr>
2022-06-20 14:44:49 +0200

    Repas

--- src/Repas.py
+++ src/Repas.py
@@ -4,14 +4,35 @@ Created on Jun 20, 2022
 @author: valerio
 '''
 
-class MyClass(object):
+class Repas:
     '''
     classdocs
     '''
 
 
-    def __init__(self, params):
+    def __init__(self, entree, plat, dessert):
         '''
         Constructor
         '''
+        self.set_entree(entree)
+        self.set_plat(plat)
+        self.set_dessert(dessert)
+            
+    def set_entree(self,entree):
+        self.entree = entree
         
+    def set_plat(self,plat):
+        self.plat = plat
+        
+    def set_dessert(self,dessert):
+        self.dessert = dessert
+        
+    def get_entree(self):
+        return self.entree
+    
+    def get_plat(self):
+        return self.plat
+    
+    def get_dessert(self):
+        return self.dessert
+ 
