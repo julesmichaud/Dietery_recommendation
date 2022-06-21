517ffde72202f87a00725cf2c9ebcfba0b77c2ee
Aurélien <aurelien.g747@gmail.com>
2022-06-21 11:28:48 +0200

    Separated classes into separate packages

---
+++ src/Main.py
@@ -0,0 +1,16 @@
+'''
+Created on 21 juin 2022
+
+@author: Aurélien
+'''
+
+class Main(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
