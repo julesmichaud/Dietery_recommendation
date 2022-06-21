117974bef5c173fc5b75580275f1eb1f1c5d0003
Aurélien <aurelien.g747@gmail.com>
2022-06-21 08:54:55 +0200

    Added Coefficients class, minor changes

---
+++ kolmogorov/Coefficients.py
@@ -0,0 +1,19 @@
+'''
+Created on 21 juin 2022
+
+@author: Aurélien
+'''
+
+class Coefficients(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.normalization_kolmogorov_ingredient = 1
+        self.normalization_kolmogorov_recipe = 1
+        #etc... incomplete list, which values have to be updated according to our needs
--- kolmogorov/Kolmogorov.py
+++ kolmogorov/Kolmogorov.py
@@ -5,6 +5,8 @@ Created on Jun 20, 2022
 '''
 from src import Ingredient
 from math import log2
+from math import floor
+import kolmogorov.Coefficients.Coefficients as coefs
 
 class Kolmogorov(object):
     '''
@@ -16,16 +18,16 @@ class Kolmogorov(object):
         Constructor
         '''
     
-    def kolmogorov_aliment(self, ingredient):
-        return log2((ingredient.local_availability_period[1]-ingredient.local_availability_period[0])/365) - log2(1/365)
+    def kolmogorov_ingredient(self, ingredient):
+        return floor((log2((ingredient.local_availability_period[1]-ingredient.local_availability_period[0])/365) - log2(1/365))/coefs.normalization_kolmogorov_ingredient)
         #Si un ingrédient n'existe qu'un jour par an, il est très inattendu, sa complexité souhaitée est donc log2(1/365) - log2(1/365) = 0
         #Si un ingrédient existe au contraire toute l'année, on veut qu'il ait une grande complexité soit log2(365/365) - log2(1/365) = log2(365) = 8,5 environ
     
-    def kolmogorov_recette(self, recipe):
+    def kolmogorov_recipe(self, recipe):
         complexity = 0
         for ingredient in recipe.get_ingredients():
             complexity += self.kolmogorov_aliment(ingredient)
-        return complexity
+        return complexity/coefs.normalization_kolmogorov_recipe
     
     def maj_weights(self, environnement, history, keyword_weights_pairs): #TODO
         return 1
