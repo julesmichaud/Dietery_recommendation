03b01948e9a74d122c70e640e3ff0cad845f839c
Aurélien <aurelien.g747@gmail.com>
2022-06-21 10:42:34 +0200

    Updated and simplified Coefficients and Kolmogorov

--- kolmogorov/Kolmogorov.py
+++ kolmogorov/Kolmogorov.py
@@ -3,9 +3,9 @@ Created on Jun 20, 2022
 
 @author: Aurélien
 '''
-from src import Ingredient
+#from src import Ingredient
 from math import log2
-from math import floor
+from math import ceil
 import kolmogorov.Coefficients.Coefficients as coefs
 
 class Kolmogorov(object):
@@ -19,7 +19,8 @@ class Kolmogorov(object):
         '''
     
     def kolmogorov_ingredient(self, ingredient):
-        return floor((log2((ingredient.local_availability_period[1]-ingredient.local_availability_period[0])/365) - log2(1/365))/coefs.normalization_kolmogorov_ingredient)
+        nbr_of_days = (ingredient.local_availability_period[1]-ingredient.local_availability_period[0]).days
+        return ceil((log2(nbr_of_days/365) - log2(1/365))/coefs.normalization_kolmogorov_ingredient)
         #Si un ingrédient n'existe qu'un jour par an, il est très inattendu, sa complexité souhaitée est donc log2(1/365) - log2(1/365) = 0
         #Si un ingrédient existe au contraire toute l'année, on veut qu'il ait une grande complexité soit log2(365/365) - log2(1/365) = log2(365) = 8,5 environ
     
