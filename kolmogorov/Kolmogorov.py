73e7121e35c6f69bbb4db3d24bea3be22d3997b0
Aurélien <aurelien.g747@gmail.com>
2022-06-24 16:15:29 +0200

    Amended Kolmogorov, debugged main

--- kolmogorov/Kolmogorov.py
+++ kolmogorov/Kolmogorov.py
@@ -7,6 +7,10 @@ Created on 20 Juin, 2022
 from math import log2, sqrt
 import kolmogorov.Coefficients.Coefficients as coefs
 
+##Important
+#Doxygen pour générer la doc des fonctions !!
+#import qrcode pour générer le qrcode vers le git sur le poster !!
+
 class Kolmogorov(object):
     '''
     classdocs
@@ -20,14 +24,14 @@ class Kolmogorov(object):
     def ingredient_availability_score(self, nbr_of_days):
         return log2(365/nbr_of_days)/coefs.ingredient_availability_coef #ici on peut mettre ce coef à 1 et pondérer les autres relativement
     
-    def personal_occurence_score(self, nbr_of_personal_occurrences, filter_quality_factor):
+    def personal_occurence_score(self, nbr_of_personal_occurrences):
         x = nbr_of_personal_occurrences
-        q = filter_quality_factor
-        return log2(0.5 + 1/sqrt((1-(x-0.1)**2)**2+((x-0.1)/q)**2))/coefs.personal_occurence_coef
+        #q = filter_quality_factor
+        #return log2(0.5 + 1/sqrt((1-(x-0.1)**2)**2+((x-0.1)/q)**2))/coefs.personal_occurence_coef
     
     def popularity_score(self, popularity_frequency):
         foc = popularity_frequency
-        return log2(1+foc)/coefs.popularity_coef
+        return log2(1/foc)/coefs.popularity_coef
     
     def kolmogorov_ingredient(self, ingredient, history): #Criteria : in season / physical distance to supermarket / nature of the ingredient
         nbr_of_days = (ingredient.local_availability_period[1]-ingredient.local_availability_period[0]).days #Number of days of availability of the ingredient during the year
--- src/Main.py
+++ src/Main.py
@@ -9,8 +9,6 @@ Created on 22 juin 2022
 #    - If it wants to generate menus, ask how many the user wants
 # - After being done with a menu, always redirect to the previous menu. Always offer a "go back" option
 from click._compat import raw_input
-from asyncio.tasks import sleep
-from comtypes._comobject import catch_errors
 from src import meal_generator
 
 def is_back(user_in):
@@ -27,40 +25,45 @@ def is_exit(user_in):
 
 def meal_arg_input():
     number_str = raw_input("Please indicate how many menus you would like to generate\n")
-    if(str.lower(number_str).__eq__("back")):
-        return -42
-    else:
-        try:
-            number_int = int(number_str)
-        except:
-            print("Please type a number")
-            number_int = meal_arg_input()
-        if(number_int<=0):
-            print("Please enter a strictly positive number")
-            number_int = meal_arg_input()
-        return number_int
+    if(is_exit(number_str)):
+        quit()
+    if(is_back(number_str)):
+        back_input_after_2()
+        quit()
+    try:
+        number_int = int(number_str)
+    except:
+        print("Please type a number or command")
+        number_int = meal_arg_input()
+    if(number_int<=0):
+        print("Please enter a strictly positive number")
+        number_int = meal_arg_input()
+    return number_int
     
 def get_number_of_menus():
     return number_of_menus
 
-def wrong_or_back_input():
+def back_input_after_2():
     user_in = raw_input("Type '1' to change your parameters and '2' to generate a meal. At any moment, type 'back' to go back and 'exit' to exit\n")
     if(is_back(user_in) or is_exit(user_in)):
         print("See you soon !")
-        exit(0)
+        quit()
     elif(user_in.__eq__("1")):
         print("Temporary menu placeholder")
+        meal_generator.MealGenerator()
+        quit()
         #Find a way to execute the general_menu file...
     elif(user_in.__eq__("2")):
         number_of_menus = meal_arg_input()
-        if(number_of_menus==-42):
-            wrong_or_back_input()
-        else:
-            print("Temporary meal generator placeholder, with " + str(number_of_menus) + " menu(s)")
+        
+        print("Temporary meal generator placeholder, with " + str(number_of_menus) + " menu(s)")
+        meal_generator.MealGenerator()
+        quit()
         #Find a way to execute the execute meal_generator, or create a generator and generate a menu here
     else:
         print("Unrecognized input, please try again")
-        wrong_or_back_input();
+        back_input_after_2();
+        quit()
 
 if __name__ == '__main__':
     number_of_menus = 0
@@ -69,19 +72,19 @@ if __name__ == '__main__':
     user_in = raw_input("Type '1' to change your parameters and '2' to generate a meal. At any moment, type 'back' to go back and 'exit' to exit\n")
     if(is_back(user_in) or is_exit(user_in)):
         print("See you soon !")
-        exit(0)
+        quit()
     elif(user_in.__eq__("1")):
         print("Temporary menu placeholder")
-        meal_generator.MealGenerator()
+        quit()
         #Find a way to execute the general_menu file...
     elif(user_in.__eq__("2")):
-        number_of_menus = meal_arg_input()
-        if(number_of_menus==-42):
-            wrong_or_back_input()
-        else:
-            print("Temporary meal generator placeholder, with " + str(number_of_menus) + " menu(s)")
+        number_of_menus = meal_arg_input() #Also considers "back" and "quit"
+        
+        print("Temporary meal generator placeholder, with " + str(number_of_menus) + " menu(s)")
+        meal_generator.MealGenerator()
+        quit()
         #Find a way to execute the execute meal_generator, or create a generator and generate a menu here
     else:
         print("Unrecognized input, please try again")
-        wrong_or_back_input();
-    exit(1)
+        back_input_after_2();
+        quit()
