2aaa4f1180dfec6d077c033ed1db1092108391d4
Aurélien <aurelien.g747@gmail.com>
2022-06-28 15:36:00 +0200

    Miscs

--- food/Ingredients.py
+++ food/Ingredients.py
@@ -16,7 +16,8 @@ class Ingredients(object):
         self.ingredients_list = [["concombre","tomate","salade"],["pomme de terre","tomate","topinambour","epinards"],["riz","pates","pain","quiche"],["blancs de poulet","cote de porc", "gigot d'agneau"],["pave de saumon","dos de cabillaud", "steack de thon"],["yaourt","reblochon"]]
         self.category_indexes_list = [[1],[2,3],[4,5],[6]]
         self.complexity_list = [[1.3,2.,1.4],[0.4,0.5,4.6,1.2],[0.39,0.35,0.8,1.8],[0.6,0.9,1.9],[0.7,2.3,3.1],[0.5,1.2]]
-        
+    
+    @staticmethod    
     def get_type(self,target_ingredient):
         ingredients_list = [["concombre","tomate","salade"],["pomme de terre","tomate","topinambour","epinards"],["riz","pates","pain","quiche"],["blancs de poulet","cote de porc", "gigot d'agneau"],["pave de saumon","dos de cabillaud", "steack de thon"],["yaourt","reblochon"]]
         for type in ingredients_list:
@@ -26,10 +27,12 @@ class Ingredients(object):
         print("No such ingredient")
         return None
     
+    @staticmethod
     def get_type_from_index(self,type_index):
         ingredients_list = [["concombre","tomate","salade"],["pomme de terre","tomate","topinambour","epinards"],["riz","pates","pain","quiche"],["blancs de poulet","cote de porc", "gigot d'agneau"],["pave de saumon","dos de cabillaud", "steack de thon"],["yaourt","reblochon"]]
         return ingredients_list[type_index]
     
+    @staticmethod
     def get_type_index(self, target_ingredient):
         ingredients_list = [["concombre","tomate","salade"],["pomme de terre","tomate","topinambour","epinards"],["riz","pates","pain","quiche"],["blancs de poulet","cote de porc", "gigot d'agneau"],["pave de saumon","dos de cabillaud", "steack de thon"],["yaourt","reblochon"]]
         for i in range(len(ingredients_list)):
@@ -39,6 +42,7 @@ class Ingredients(object):
         print("No such ingredient")
         return None
     
+    @staticmethod
     def get_category_indexes(self,target_ingredient):
         category_indexes_list = [[0],[1,2],[3,4],[5]]
         type_index = self.get_type_index(self,target_ingredient)
@@ -47,10 +51,12 @@ class Ingredients(object):
                 return category_indexes
         return None
     
+    @staticmethod
     def get_complexity(self, target_ingredient):
         ingredients_list = [["concombre","tomate","salade"],["pomme de terre","tomate","topinambour","epinards"],["riz","pates","pain","quiche"],["blancs de poulet","cote de porc", "gigot d'agneau"],["pave de saumon","dos de cabillaud", "steack de thon"],["yaourt","reblochon"]]
         category_indexes_list = [[0],[1,2],[3,4],[5]]
         complexity_list = [[1.3,2.,1.4],[0.4,0.5,4.6,1.2],[0.39,0.35,0.8,1.8],[0.6,0.9,1.9],[0.7,2.3,3.1],[0.5,1.2]]
+        
         i = self.get_type_index(self, target_ingredient)
         type = ingredients_list[i]
         j = -1
@@ -58,4 +64,11 @@ class Ingredients(object):
             if(type[k] == target_ingredient):
                 j=k
                 break
-        return complexity_list[i][j]
+        score = complexity_list[i][j]
+        #aliment_expected_complexity = Kolmogorov.Kolmogorov.kolmogorov_ingredient(Kolmogorov.Kolmogorov, target_ingredient, History.History)
+        #aliment_observed_complexity = 
+        #aliment_contraint_complexity = constraint.Constraint(constraint.Constraint
+        #simplicity = aliment_expected_complexity - aliment_observed_complexity
+        #score = simplicity + aliment_contraint_complexity
+        
+        return score
